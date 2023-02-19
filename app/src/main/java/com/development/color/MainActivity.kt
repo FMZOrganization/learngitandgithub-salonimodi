556dc8b9105eb6c4473035fdfbb6b043a92f2d69
Saloni.modi <salonimodi293@gmail.com>
2023-02-19 12:11:52 -0800

    Feedback changes

--- app/src/main/java/com/development/color/MainActivity.kt
+++ app/src/main/java/com/development/color/MainActivity.kt
@@ -17,7 +17,6 @@ import android.widget.SeekBar
 import android.widget.Switch
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
-import kotlin.math.roundToInt
 
 @SuppressLint("UseSwitchCompatOrMaterialCode")
 class MainActivity : AppCompatActivity() {
@@ -97,20 +96,20 @@ class MainActivity : AppCompatActivity() {
         })
 
         redSwitch.setOnCheckedChangeListener { _, isChecked ->
-            setColorSwitchListener(redSwitch, redSeekBar, redEditText, R.color.red,isChecked )
+            setColorSwitchListener(redSwitch, redSeekBar, redEditText, R.color.red,isChecked)
         }
 
         greenSwitch.setOnCheckedChangeListener { _, isChecked ->
-            setColorSwitchListener(greenSwitch, greenSeekBar, greenEditText, R.color.green, isChecked )
+            setColorSwitchListener(greenSwitch, greenSeekBar, greenEditText, R.color.green, isChecked)
         }
 
         blueSwitch.setOnCheckedChangeListener { _, isChecked ->
-            setColorSwitchListener(blueSwitch, blueSeekBar, blueEditText, R.color.blue,isChecked )
+            setColorSwitchListener(blueSwitch, blueSeekBar, blueEditText, R.color.blue,isChecked)
         }
 
         redEditText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                setupEditText(s, redSeekBar, colorBox)
+                setupEditText(s,redEditText, redSeekBar, colorBox)
             }
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
@@ -118,7 +117,7 @@ class MainActivity : AppCompatActivity() {
 
         greenEditText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                setupEditText(s, greenSeekBar, colorBox)
+                setupEditText(s, greenEditText,greenSeekBar, colorBox)
             }
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
@@ -126,7 +125,7 @@ class MainActivity : AppCompatActivity() {
 
         blueEditText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                setupEditText(s, blueSeekBar, colorBox)
+                setupEditText(s,blueEditText, blueSeekBar, colorBox)
             }
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
@@ -143,6 +142,9 @@ class MainActivity : AppCompatActivity() {
             redSwitch.isChecked = true
             greenSwitch.isChecked = true
             blueSwitch.isChecked = true
+            redSwitch.isEnabled = true
+            greenSwitch.isEnabled = true
+            blueSwitch.isEnabled = true
             updateColorBox(colorBox, 0, 0, 0)
         }
 
@@ -157,11 +159,22 @@ class MainActivity : AppCompatActivity() {
         colorBox.setBackgroundColor(color)
     }
 
-    fun setupEditText(input: CharSequence?, seekBar: SeekBar, colorBox: View) {
-        val value = input?.toString()?.toFloatOrNull() ?: 0f
+    fun setupEditText(s: CharSequence?, editText: EditText, seekBar: SeekBar, colorBox: View) {
+
+        val value = s?.toString()?.toFloatOrNull() ?: 0f
         val progress = (value.coerceIn(0f, 1f) * 100 + 0.5f).toInt().coerceAtLeast(1)
         seekBar.progress = progress
         updateColorBox(colorBox, redSeekBar.progress, greenSeekBar.progress, blueSeekBar.progress)
+        if (s?.length ?: 0 > 0) {
+            val floatValue = s.toString().toFloat()
+            if (floatValue < 0) {
+                // The input is less than 0, set it to 0
+                editText.setText("0")
+            } else if (floatValue > 1) {
+                // The input is greater than 1, set it to 1
+                editText.setText("1")
+            }
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
